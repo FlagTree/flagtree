fd4280e8580eb328851585cf630c37778a35f70b
Keren Zhou <kerenzhou@openai.com>
2024-06-13 13:17:31 -0400

    [BUILD] Change hatchet as an extra dependency (#4138)

--- python/setup.py
+++ python/setup.py
@@ -595,7 +595,7 @@ def get_entry_points():
 
 
 def get_install_requires():
-    install_requires = ["filelock", "llnl-hatchet"]
+    install_requires = ["filelock"]
     return install_requires
 
 
@@ -668,6 +668,7 @@ setup(
             "numpy",
             "pytest",
             "scipy>=1.7.1",
+            "llnl-hatchet",
         ],
         "tutorials": [
             "matplotlib",
