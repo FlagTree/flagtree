2a292b4685f709d16bdde0d8e7c9657152c065d3
AdvancedCompiler <canaanleen@163.com>
2025-05-30 16:44:50 +0800

    code format

--- python/test/operators/test_expression_restructuring.py
+++ python/test/operators/test_expression_restructuring.py
@@ -4,7 +4,8 @@ import torch
 
 import pytest
 
-VEC_SHAPES = [[64, 640], [32,128], [128,256]]
+VEC_SHAPES = [[64, 640], [32, 128], [128, 256]]
+
 
 def custom_rand_strided(shape, strides, device, dtype, seed=0):
     torch.manual_seed(seed)
@@ -12,16 +13,18 @@ def custom_rand_strided(shape, strides, device, dtype, seed=0):
     storage = torch.randn(total_size, device=device, dtype=dtype)
     return torch.as_strided(storage, size=shape, stride=strides)
 
+
 def torch_equivalent(arg_0, arg_1, arg_2, arg_3):
-    reshaped_arg_0 = arg_0.view(arg_2.shape[0], arg_2.shape[0], arg_2.shape[2])  
-    reshaped_arg_3 = arg_3.squeeze(-1)     
-    tmp0 = -reshaped_arg_0                 
-    tmp4 = arg_1 * arg_2                   
-    tmp7 = reshaped_arg_3 + 1e-06          
-    tmp8 = tmp4 / tmp7.unsqueeze(-1)       
-    tmp9 = tmp8 / tmp7.unsqueeze(-1)       
+    reshaped_arg_0 = arg_0.view(arg_2.shape[0], arg_2.shape[0], arg_2.shape[2])
+    reshaped_arg_3 = arg_3.squeeze(-1)
+    tmp0 = -reshaped_arg_0
+    tmp4 = arg_1 * arg_2
+    tmp7 = reshaped_arg_3 + 1e-06
+    tmp8 = tmp4 / tmp7.unsqueeze(-1)
+    tmp9 = tmp8 / tmp7.unsqueeze(-1)
     result = tmp0 * tmp9
     return result
+
 
 @triton.jit
 def expression_restructuring_function_test(in_ptr0, in_ptr1, in_ptr2, in_ptr3, out_ptr2, rnumel):
@@ -50,16 +53,12 @@ def expression_restructuring_function_test(in_ptr0, in_ptr1, in_ptr2, in_ptr3, o
 def test_accruacy_kernel(vec_shape):
     x = vec_shape[0]
     y = vec_shape[1]
-    arg_0 = custom_rand_strided((x * x, y), (y, 1), dtype=torch.float32,device='cuda')
-    arg_1 = custom_rand_strided((y,), (1,), dtype=torch.float32,device='cuda')
-    arg_2 = custom_rand_strided((x, x, y), (x * y, y, 1), dtype=torch.float32,device='cuda')
-    arg_3 = custom_rand_strided((x, x, 1), (x, 1, 1), dtype=torch.float32,device='cuda')
-    triton_result = custom_rand_strided((x, x, y), (x * y, y, 1), dtype=torch.float32,device='cuda')
-    grid = lambda meta: (x*x, )
-    expression_restructuring_function_test[grid](arg_0, arg_1, arg_2, arg_3,  triton_result, y)
+    arg_0 = custom_rand_strided((x * x, y), (y, 1), dtype=torch.float32, device='cuda')
+    arg_1 = custom_rand_strided((y, ), (1, ), dtype=torch.float32, device='cuda')
+    arg_2 = custom_rand_strided((x, x, y), (x * y, y, 1), dtype=torch.float32, device='cuda')
+    arg_3 = custom_rand_strided((x, x, 1), (x, 1, 1), dtype=torch.float32, device='cuda')
+    triton_result = custom_rand_strided((x, x, y), (x * y, y, 1), dtype=torch.float32, device='cuda')
+    grid = lambda meta: (x * x, )
+    expression_restructuring_function_test[grid](arg_0, arg_1, arg_2, arg_3, triton_result, y)
     torch_result = torch_equivalent(arg_0, arg_1, arg_2, arg_3)
     torch.testing.assert_close(triton_result, torch_result)
-    
-
-    
-
