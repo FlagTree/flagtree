4d1993035c4ab9bf87293cc84381fa9078fb15a5
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-06-16 15:03:39 +0800

    [install & compile refactor] refactor setup (#19)
    * [init refactor] refactor setup
    
    * add attr
    
    * [fix bug]
    
    * polish code
    
    * [fix ci]
    
    * [fix bugs]
    
    * fix bug
    
    ---------
    
    Co-authored-by: StrongSpoon <strongspoon@outlook.com>

--- python/setup.py
+++ python/setup.py
@@ -28,7 +28,7 @@ from wheel.bdist_wheel import bdist_wheel
 
 import pybind11
 
-import setup_helper as helper
+from setup_tools import setup_helper as helper
 
 
 @dataclass
@@ -423,6 +423,7 @@ class CMakeBuild(build_ext):
             "-DTRITON_CODEGEN_BACKENDS=" + ';'.join([b.name for b in backends if not b.is_external]),
             "-DTRITON_PLUGIN_DIRS=" + ';'.join([b.src_dir for b in backends if b.is_external])
         ]
+        cmake_args += helper.get_backend_cmake_args(build_ext=self)
         if lit_dir is not None:
             cmake_args.append("-DLLVM_EXTERNAL_LIT=" + lit_dir)
         cmake_args.extend(thirdparty_cmake_args)
@@ -487,6 +488,7 @@ class CMakeBuild(build_ext):
         subprocess.check_call(["cmake", self.base_dir] + cmake_args, cwd=cmake_dir, env=env)
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=cmake_dir)
         subprocess.check_call(["cmake", "--build", ".", "--target", "mlir-doc"], cwd=cmake_dir)
+        helper.install_extension(build_ext=self)
 
 
 nvidia_version_path = os.path.join(get_base_dir(), "cmake", "nvidia-toolchain-version.json")
@@ -611,7 +613,7 @@ class plugin_install(install):
     def run(self):
         add_links()
         install.run(self)
-        helper.post_install(self)
+        helper.post_install()
 
 
 class plugin_develop(develop):
@@ -619,7 +621,7 @@ class plugin_develop(develop):
     def run(self):
         add_links()
         develop.run(self)
-        helper.post_install(self)
+        helper.post_install()
 
 
 class plugin_bdist_wheel(bdist_wheel):
@@ -627,7 +629,7 @@ class plugin_bdist_wheel(bdist_wheel):
     def run(self):
         add_links()
         bdist_wheel.run(self)
-        helper.post_install(self)
+        helper.post_install()
 
 
 class plugin_egginfo(egg_info):
@@ -635,7 +637,7 @@ class plugin_egginfo(egg_info):
     def run(self):
         add_links()
         egg_info.run(self)
-        helper.post_install(self)
+        helper.post_install()
 
 
 package_data_tools = helper.get_package_data_tools()
