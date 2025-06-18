5498b6870bb58c324fa6e5ef10b824e9eac5e19e
ikushare <2697533527@qq.com>
2025-06-18 16:37:11 +0800

    The extension of the external math library math.fmod requires adding definitions in libdevice.py of AIPU

---
+++ test/MathExt/test_fmod.py
@@ -0,0 +1,32 @@
+import torch
+import triton
+import triton.language as tl
+from triton.language.extra.aipu import libdevice
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+
+@triton.jit()
+def asin_kernel(
+    x_ptr,
+    y_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    z = libdevice.fmod(x, y)
+    tl.store(y_ptr + offsets, z, mask=mask)
+
+
+torch.manual_seed(0)
+size = 98432
+x = torch.rand(size, device=DEVICE)
+output_triton = torch.rand(size, device=DEVICE)
+n_elements = x.numel()
+grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+asin_kernel[grid](x, output_triton, n_elements, BLOCK_SIZE=1024)
