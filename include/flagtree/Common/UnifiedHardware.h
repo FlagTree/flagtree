63337036de68ab06e8c691811627206e366f47a6
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-07-08 11:29:07 +0800

    [Common]Add UnifiedHardware  (#39)
    * add .h
    
    * add .h
    
    * add uh, needed to fix mod.set_attr(uh_aipu_ptr, ptr_attr)
    
    * [ARCH] Add UnifiedHardware to config backend
    
    * [ARCH] Add UnifiedHardware to config backend
    
    * [CI/CD] Limit aipu build MAX_JOBS to 16
    
    * [ARCH] Add UnifiedHardware to config backend
    
    * init unifiedHardware
    
    * add Cmake func
    
    * code style
    
    * code style
    
    * fix bug
    
    * add Registrar
    
    * polish code
    
    ---------
    
    Co-authored-by: i3wanna2 <15910307812@163.com>
    Co-authored-by: zhengyang <zhengyang@baai.ac.cn>

---
+++ include/flagtree/Common/UnifiedHardware.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+
+namespace mlir {
+namespace flagtree {
+// this is the unified hardware abstraction for hardware
+// to determined if these abstraction is specified, using std::optional is
+// needed using in passes: if(uh_flagtree->xxx()){...}
+
+class UnifiedHardware {
+
+public:
+  ~UnifiedHardware() = default;
+  UnifiedHardware() = default;
+#ifdef FLAGTREE_BACKEND
+  static bool registered;
+  int getDMATag();
+  int getSharedMemoryTag();
+  std::string getFlagTreeBackend() { return FLAGTREE_BACKEND; }
+#else
+  static constexpr bool registered = false;
+  void *getDMATag() { return nullptr; }
+  void *getSharedMemoryTag() { return nullptr; }
+  std::string getFlagTreeBackend() { return "default"; }
+#endif
+};
+
+std::unique_ptr<UnifiedHardware> createUnifiedHardwareManager();
+
+} // namespace flagtree
+} // namespace mlir
+
+#define SET_REGISTER_FLAG(_Ty, FLAG) bool _Ty::registered = FLAG;
+
+#define FLAGTREE_REGISTRAR_GET(_Ty, _Fn, _VAL)                                 \
+  decltype(_VAL) _Ty::get##_Fn() { return static_cast<decltype(_VAL)>(_VAL); }
+
+#ifdef FLAGTREE_BACKEND
+#define FLAGTREE_REGISTRAR(fn_name, _VAL)                                      \
+  using UnifiedHardwareType = mlir::flagtree::UnifiedHardware;                 \
+  FLAGTREE_REGISTRAR_GET(UnifiedHardwareType, fn_name, _VAL)                   \
+  SET_REGISTER_FLAG(UnifiedHardwareType, true)
+#else
+#define FLAGTREE_REGISTRAR(...)
+#endif
