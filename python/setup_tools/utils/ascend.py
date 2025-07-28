2229524ec2865e7e2e5ee1fac181fcd25b75693d
yrl <2535184404@qq.com>
2025-07-28 11:10:02 +0800

    fix

--- python/setup_tools/utils/ascend.py
+++ python/setup_tools/utils/ascend.py
@@ -93,94 +93,82 @@ def append_at_file_end(filepath, import_lines):
     return False
 
 
-def post_install():
-    import site
-    install_dir = site.getsitepackages()[0]
-    install_dir = os.path.join(install_dir, "triton")
-    init_path = os.path.join(install_dir, "__init__.py")
-    patched_content = """
-import sys
-from .triton_patch.language import _utils as ascend_utils
-sys.modules['triton.language._utils'] = ascend_utils
-from .triton_patch.compiler import compiler as ascend_compiler
-sys.modules['triton.compiler.compiler'] = ascend_compiler
-from .triton_patch.compiler import code_generator as ascend_code_generator
-sys.modules['triton.compiler.code_generator'] = ascend_code_generator
-from .triton_patch.compiler import errors as ascend_errors
-sys.modules['triton.compiler.errors'] = ascend_errors
-from .triton_patch.runtime import autotuner as ascend_autotuner
-sys.modules['triton.runtime.autotuner'] = ascend_autotuner
-from .triton_patch import testing as ascend_testing
-sys.modules['triton.testing'] = ascend_testing
-"""
-    insert_at_file_start(init_path, patched_content)
+# def post_install():
+#     import site
+#     install_dir = site.getsitepackages()[0]
+#     install_dir = os.path.join(install_dir, "triton")
+#     init_path = os.path.join(install_dir, "__init__.py")
+#     patched_content = """
+# import sys
+# from .triton_patch.language import _utils as ascend_utils
+# sys.modules['triton.language._utils'] = ascend_utils
+# from .triton_patch.compiler import compiler as ascend_compiler
+# sys.modules['triton.compiler.compiler'] = ascend_compiler
+# from .triton_patch.compiler import code_generator as ascend_code_generator
+# sys.modules['triton.compiler.code_generator'] = ascend_code_generator
+# from .triton_patch.compiler import errors as ascend_errors
+# sys.modules['triton.compiler.errors'] = ascend_errors
+# from .triton_patch.runtime import autotuner as ascend_autotuner
+# sys.modules['triton.runtime.autotuner'] = ascend_autotuner
+# from .triton_patch import testing as ascend_testing
+# sys.modules['triton.testing'] = ascend_testing
+# """
+#     insert_at_file_start(init_path, patched_content)
 
-    content_to_append = """
-from .triton_patch.language.core import dot, gather, insert, subview
-from .triton_patch.language.standard import flip
-from .triton_patch.language.math import umulhi, exp, exp2, log, log2, cos, sin, sqrt, sqrt_rn, rsqrt, div_rn, erf, tanh, floor, ceil
-from . import language
+#     content_to_append = """
+# from .triton_patch.language.core import dot, gather, insert, subview
+# from .triton_patch.language.standard import flip
+# from .triton_patch.language.math import umulhi, exp, exp2, log, log2, cos, sin, sqrt, sqrt_rn, rsqrt, div_rn, erf, tanh, floor, ceil
+# from . import language
 
-language.dot = dot
-language.flip = flip
-language.gather = gather
-language.insert = insert
-language.subview = subview
+# language.dot = dot
+# language.flip = flip
+# language.gather = gather
+# language.insert = insert
+# language.subview = subview
 
-# from .triton_patch.language.core import dtype, pointer_type, block_type, function_type
-# language.core.dtype = dtype
-# language.core.pointer_type = pointer_type
-# language.core.block_type = block_type
-# language.core.function_type = function_type
+# # from .triton_patch.language.core import dtype, pointer_type, block_type, function_type
+# # language.core.dtype = dtype
+# # language.core.pointer_type = pointer_type
+# # language.core.block_type = block_type
+# # language.core.function_type = function_type
 
-from .triton_patch.language.semantic import arange, floordiv
-language.semantic.arange = arange
-language.semantic.floordiv = floordiv
+# from .triton_patch.language.semantic import arange, floordiv
+# language.semantic.arange = arange
+# language.semantic.floordiv = floordiv
 
-language.umulhi = umulhi
-language.exp = exp
-language.exp2 = exp2
-language.log = log
-language.log2 = log2
-language.cos = cos
-language.sin = sin
-language.sqrt = sqrt
-language.sqrt_rn = sqrt_rn
-language.rsqrt = rsqrt
-language.div_rn = div_rn
-language.erf = erf
-language.tanh = tanh
-language.floor = floor
-language.ceil = ceil
-language.math.umulhi = umulhi
-language.math.exp = exp
-language.math.exp2 = exp2
-language.math.log = log
-language.math.log2 = log2
-language.math.cos = cos
-language.math.sin = sin
-language.math.sqrt = sqrt
-language.math.sqrt_rn = sqrt_rn
-language.math.rsqrt = rsqrt
-language.math.div_rn = div_rn
-language.math.erf = erf
-language.math.tanh = tanh
-language.math.floor = floor
-language.math.ceil = ceil
-"""
-    append_at_file_end(init_path, content_to_append)
-
-
-def get_ascend_patch_packages(backends):
-    packages = []
-    # packages += get_language_extra_packages()
-    packages += [
-        "triton/triton_patch",
-        "triton/triton_patch/language",
-        "triton/triton_patch/compiler",
-        "triton/triton_patch/runtime",
-    ]
-    return packages
+# language.umulhi = umulhi
+# language.exp = exp
+# language.exp2 = exp2
+# language.log = log
+# language.log2 = log2
+# language.cos = cos
+# language.sin = sin
+# language.sqrt = sqrt
+# language.sqrt_rn = sqrt_rn
+# language.rsqrt = rsqrt
+# language.div_rn = div_rn
+# language.erf = erf
+# language.tanh = tanh
+# language.floor = floor
+# language.ceil = ceil
+# language.math.umulhi = umulhi
+# language.math.exp = exp
+# language.math.exp2 = exp2
+# language.math.log = log
+# language.math.log2 = log2
+# language.math.cos = cos
+# language.math.sin = sin
+# language.math.sqrt = sqrt
+# language.math.sqrt_rn = sqrt_rn
+# language.math.rsqrt = rsqrt
+# language.math.div_rn = div_rn
+# language.math.erf = erf
+# language.math.tanh = tanh
+# language.math.floor = floor
+# language.math.ceil = ceil
+# """
+#     append_at_file_end(init_path, content_to_append)
 
 
 def get_ascend_patch_package_dir(backends):
