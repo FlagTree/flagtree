51edd69c375d65c197b4cdf1ecda24ea9c1bf5b0
zhengyang <zhengyang@baai.ac.cn>
2025-07-31 10:55:29 +0000

    update code_generator ast_to_ttir

--- third_party/iluvatar/backend/backend_impl.py
+++ third_party/iluvatar/backend/backend_impl.py
@@ -6,6 +6,7 @@ class IluvatarBackend():
     def kernel_suffix(signature, specialization):
         # suffix format:
         # <argid><'c' if equal to 1><'d' if divisible by 16><'e' if divisible by 8>
+        print("=============")
         suffix = ''
         for i, _ in enumerate(signature):
             suffix += str(i)
@@ -22,7 +23,7 @@ class IluvatarBackend():
         from triton.compiler.code_generator import _get_fn_file_line, CodeGenerator
         from triton.language import str_to_ty
         from triton import language
-        
+
         attrs = specialization.attrs
         # create kernel prototype
         cst_key = lambda i: fn.arg_names.index(i) if isinstance(i, str) else i
@@ -32,10 +33,10 @@ class IluvatarBackend():
         function_name = fn.repr(specialization)
         tys = list(specialization.signature.values())
         new_constants = {k: True if k in tys and tys[k] == "i1" else 1 for k in attrs.equal_to_1}
-        new_attrs = {k: [("tt.divisibility", 16)] for k in attrs.divisible_by_16}
-        for k in attrs.divisible_by_8:
+        new_attrs = {k[0]: [("tt.corex_stride", k[1])] for k in attrs.corexLoad.items()}
+        for k in attrs.divisible_by_16:
             attr = new_attrs[k] if k in new_attrs else []
-            attr.append(("tt.max_divisibility", 8))
+            attr.append(("tt.divisibility", 16))
             new_attrs[k] = attr
 
         all_constants = constants.copy()
@@ -45,13 +46,14 @@ class IluvatarBackend():
 
         prototype = language.function_type([], arg_types)
         generator = CodeGenerator(context, prototype, gscope=gscope, constants=all_constants, function_name=function_name,
-                                jit_fn=fn, attributes=new_attrs, is_kernel=True, file_name=file_name,
-                                begin_line=begin_line, options=options, codegen_fns=codegen_fns)
+                                  jit_fn=fn, attributes=new_attrs, is_kernel=True, file_name=file_name,
+                                  begin_line=begin_line, options=options, codegen_fns=codegen_fns)
         generator.visit(fn.parse())
 
         ret = generator.module
         # module takes ownership of the context
         ret.context = context
         return ret
+
 
 register_backend("iluvatar", IluvatarBackend())
