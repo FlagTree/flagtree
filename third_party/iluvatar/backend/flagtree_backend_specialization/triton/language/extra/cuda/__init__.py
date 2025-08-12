1401229c6534bfc3fd4a5faa0a6f7b7062346b7e
toudefu <2696810816@qq.com>
2025-08-12 16:32:45 +0800

    fix: delete bmm_matmul

--- third_party/iluvatar/backend/flagtree_backend_specialization/triton/language/extra/cuda/__init__.py
+++ third_party/iluvatar/backend/flagtree_backend_specialization/triton/language/extra/cuda/__init__.py
@@ -1,7 +1,4 @@
 def language_extra_cuda_get_all():
-    from triton.language.extra.cuda import libdevice
-    from triton.language.extra.cuda.utils import (globaltimer, num_threads, num_warps, smid,
-                                                  convert_custom_float8_sm70, convert_custom_float8_sm80)
     __all__ = [
                 "libdevice",
                 #"globaltimer",
--- third_party/iluvatar/backend/flagtree_backend_specialization/triton/ops/__init__.py
+++ third_party/iluvatar/backend/flagtree_backend_specialization/triton/ops/__init__.py
@@ -1,8 +1,6 @@
 def ops_get_all():
-    from .bmm_matmul import _bmm, bmm
-
     __all__ = [
-                "blocksparse", "_cross_entropy", "cross_entropy", "_matmul", "matmul", "_bmm", "bmm", "attention",
+                "blocksparse", "_cross_entropy", "cross_entropy", "_matmul", "matmul", "attention",
                 "get_higher_dtype"
               ]
     return __all__
