ff382a651d279f6adc70f88236d38d651db87943
toudefu <2696810816@qq.com>
2025-08-13 17:00:52 +0800

    fix cache: cancel early remove

--- third_party/iluvatar/backend/flagtree_backend_specialization/triton/runtime/cache.py
+++ third_party/iluvatar/backend/flagtree_backend_specialization/triton/runtime/cache.py
@@ -3,7 +3,6 @@ def get_temp_path(fileCacheManager, pid, rnd_id, filename):
     temp_dir = os.path.join(fileCacheManager.cache_dir, f"tmp.pid_{pid}_{rnd_id}")
     os.makedirs(temp_dir, exist_ok=True)
     temp_path = os.path.join(temp_dir, filename)
-    os.removedirs(temp_dir)
     return temp_path
 
 
