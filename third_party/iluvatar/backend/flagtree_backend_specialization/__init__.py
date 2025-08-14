ff661e2003e8f29efadade5ecf3447ca9d422e98
toudefu <2696810816@qq.com>
2025-08-14 15:17:16 +0800

    add bmm_matmul

--- third_party/iluvatar/backend/flagtree_backend_specialization/__init__.py
+++ third_party/iluvatar/backend/flagtree_backend_specialization/__init__.py
@@ -3,6 +3,7 @@ from .triton.compiler.compiler import *
 from .triton.language.extra.cuda import *
 from .triton.language.semantic import *
 from .triton.ops import *
+from .triton.ops.bmm_matmul import *
 from .triton.ops.flash_attention import *
 from .triton.ops.matmul import *
 from .triton.ops.matmul_perf_model import *
@@ -16,7 +17,7 @@ __all__ = [
             "kernel_suffix_by_divisibility", "generate_new_attrs_in_ast_to_ttir", "init_corexLoad", "to_dict_corexLoad",
             "from_dict_corexLoad", "hash_AttrsDescriptor", "src_fn_hash_cache_file", "src_fn_so_path", "init_handles_n_threads",
             "language_extra_cuda_get_all", "cv_cache_modifier", "element_ty_is_bf16", "atomin_add_int64", "ops_get_all",
-            "sequence_parallel_mma_v3_dq",
+            "bmm_utils", "_bmm", "bmm", "sequence_parallel_mma_v3_dq",
             "hardware_config", "get_num_stages", "get_block_and_warps", "get_num_warps", "hasattr_corex", "get_configs_compute_bound",
             "get_nv_configs", "get_configs", "get_top_k", "get_pid_m_n", "K_is_divisible", "get_total_time_ms", "is_hasattr_corex",
             "pruned_configs_corex", "add_Autotuner_cache_fn_map", "build_best_config_hash", "load_best_config", "save_best_config",
--- third_party/iluvatar/backend/flagtree_backend_specialization/triton/ops/__init__.py
+++ third_party/iluvatar/backend/flagtree_backend_specialization/triton/ops/__init__.py
@@ -1,6 +1,8 @@
 def ops_get_all():
+    from .bmm_matmul import _bmm, bmm
+
     __all__ = [
-                "blocksparse", "_cross_entropy", "cross_entropy", "_matmul", "matmul", "attention",
+                "blocksparse", "_cross_entropy", "cross_entropy", "_matmul", "matmul", "_bmm", "bmm", "attention",
                 "get_higher_dtype"
               ]
     return __all__
