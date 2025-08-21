a3641219313ad7c767faee221e8cb3218ea4a90b
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-08-21 13:59:22 +0800

    [Build] Fix offline build BUG

--- python/setup_tools/setup_helper.py
+++ python/setup_tools/setup_helper.py
@@ -83,8 +83,10 @@ def dir_rollback(deep, base_path):
 def download_flagtree_third_party(name, condition, required=False, hock=None):
     if condition:
         submoduel = utils.flagtree_submoduels[name]
-        utils.download_module(submoduel, required)
-        if callable(hock):
+        succ = utils.download_module(submoduel, required)
+        if not succ:
+            print('[INFO] Download/Copy triton_shared failed. Skip appending triton_shared as default backend')
+        if callable(hock) and succ:
             hock(third_party_base_dir=utils.flagtree_submoduel_dir, backend=submoduel,
                  default_backends=default_backends)
 
