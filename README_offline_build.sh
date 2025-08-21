8ed7e1f802a558810dbcfda2132651b9981ab1ee
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-08-21 10:32:33 +0800

    [Build] Enhance offline build

---
+++ README_offline_build.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+echo " =================== Offline Build README ==================="
+# detect pybind11 version requirement
+PYBIND11_VERSION_FILE="cmake/pybind11-version.txt"
+if [ -f "$PYBIND11_VERSION_FILE" ]; then
+    pybind11_version=$(tr -d '\n' < "$PYBIND11_VERSION_FILE")
+    # echo "Pybind11 Version Required: $pybind11_version"
+else
+    echo "Error: version file $PYBIND11_VERSION_FILE is not exist"
+    exit 1
+fi
+
+# detect nvidia toolchain version requirement
+NV_TOOLCHAIN_VERSION_FILE="cmake/nvidia-toolchain-version.txt"
+if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
+    nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
+    # echo "Nvidia Toolchain Version Required: $nv_toolchain_version"
+else
+    echo "Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist"
+    exit 1
+fi
+
+arch=$(uname -m)
+# echo "Detected the system arch: $arch"
+
+case "$arch" in
+    x86_64)
+        arch="64"
+        ;;
+    arm64|aarch64)
+        arch="aarch64"
+        ;;
+    *)
+        ;;
+esac
+
+echo ""
+echo "This is a guide for building FlagTree in an offline environment."
+echo ""
+echo ">>>>> First, download the dependencies according to the following methods:"
+echo "You can choose two download methods:"
+echo ""
+echo "  1. Manually download:"
+echo "      NVCC should be downloaded from: https://anaconda.org/nvidia/cuda-nvcc/${nv_toolchain_version}/download/linux-${arch}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
+echo "      CUOBJBDUMP should be downloaded from: https://anaconda.org/nvidia/cuda-cuobjdump/${nv_toolchain_version}/download/linux-${arch}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
+echo "      NVDISAM should be downloaded from: https://anaconda.org/nvidia/cuda-nvdisasm/${nv_toolchain_version}/download/linux-${arch}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
+echo "      CUDART should be downloaded from: https://anaconda.org/nvidia/cuda-cudart-dev/${nv_toolchain_version}/download/linux-${arch}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
+echo "      CUPTI should be downloaded from: https://anaconda.org/nvidia/cuda-cupti/${nv_toolchain_version}/download/linux-${arch}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
+echo "      JSON library should be downloaded from: https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/include.zip"
+echo "      PYBIND11 should be downloaded from: https://github.com/pybind/pybind11/archive/refs/tags/v${pybind11_version}.tar.gz"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/pybind11-${pybind11_version}.tar.gz"
+echo "      GOOGLETEST should be downloaded from https://github.com/google/googletest/archive/refs/tags/release-1.12.1.zip"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/googletest-release-1.12.1.zip"
+echo "      (TRITON_SHARED is optional):"
+echo "      TRITON_SHARED should be downloaded from https://github.com/microsoft/triton-shared/archive/380b87122c88af131530903a702d5318ec59bb33.zip"
+echo "          and stored as: <YOUR_DOWNLOAD_DIR>/triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
+echo ""
+echo "  2. Use the script to download. The default to store the downloaded files is ~/.flagtree-offline-download,"
+echo "     you can specify the directory the store the downloaded files into: (default: ~/.flagtree-offline-download)"
+echo "       # Default"
+echo "          $ sh ./offline_build_download.sh"
+echo "       # Specify directory to store downloaded files"
+echo "          $ sh ./offline_build_download.sh <YOUR_DOWNLOAD_DIR>"
+echo ""
+echo ">>>>> Second, run the script to pack the dependencies into a .zip file. You can specify the source directory"
+echo "      providing the downloaded files. (default: ~/.flagtree-offline-download)"
+echo "       # Default"
+echo "          $ sh utils/offline_build_pack.sh"
+echo "       # Specify the input directory"
+echo "          $ sh utils/offline_build_pack.sh <YOUR_DOWNLOAD_DIR>"
+echo ""
+echo ">>>>> Third, after uploading the packed .zip file to the offline environment, run the script utils/offline_build_unpack.sh "
+echo "      to extract the dependencies to an appropriate location for FlagTree to copy. You can specify the directory to store the"
+echo "      unpacked dependencies. (default: ~/.flagtree-offline-build)"
+echo "       # Default"
+echo "          $ sh utils/offline_build_unpack.sh"
+echo "       # Specify the input & output directory"
+echo "          $ sh utils/offline_build_unpack.sh <YOUR_UNPACK_DIR>"
+echo ""
+echo ">>>>> Finally, you can proceed with the installation normally according to the README.md."
+echo "      NOTE: Set the environment variables required for offline build before running 'pip install'"
+echo "          $ export TRITON_OFFLLINE_BUILD=ON"
+echo "          $ export FLAGTREE_OFFLINE_BUILD_DIR=<UNPACK_DIR>"
+echo ""
+echo " =============================================="
---
+++ utils/offline_build_pack.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+echo " =================== Start Packing Downloaded Offline Build Files ==================="
+echo ""
+# detect pybind11 version requirement
+PYBIND11_VERSION_FILE="cmake/pybind11-version.txt"
+if [ -f "$PYBIND11_VERSION_FILE" ]; then
+    pybind11_version=$(tr -d '\n' < "$PYBIND11_VERSION_FILE")
+    echo "Pybind11 Version Required: $pybind11_version"
+else
+    echo "Error: version file $PYBIND11_VERSION_FILE is not exist"
+    exit 1
+fi
+
+# detect nvidia toolchain version requirement
+NV_TOOLCHAIN_VERSION_FILE="cmake/nvidia-toolchain-version.txt"
+if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
+    nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
+    echo "Nvidia Toolchain Version Required: $nv_toolchain_version"
+else
+    echo "Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist"
+    exit 1
+fi
+
+if [ $# -ge 1 ]; then
+    input_dir="$1"
+else
+    input_dir="~/.flagtree-offline-download"
+fi
+
+echo ""
+if [ ! -d "$input_dir" ]; then
+    echo "Creating default download directory $input_dir"
+    mkdir -p "$input_dir"
+else
+    echo "Default download directory $input_dir already exists"
+fi
+echo ""
+
+output_dir="$PWD"
+
+nvcc_file="${input_dir}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
+cuobjdump_file="${input_dir}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
+nvdisam_file="${input_dir}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
+cudart_file="${input_dir}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
+cupti_file="${input_dir}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
+json_file="${input_dir}/include.zip"
+pybind11_file="${input_dir}/pybind11-${pybind11_version}.tar.gz"
+googletest_file="${input_dir}/googletest-release-1.12.1.zip"
+triton_shared_file="${input_dir}/triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
+
+output_zip="./offline-packed-nv${nv_toolchain_version}-pybind${pybind11_version}.zip"
+
+if [ ! -d "$input_dir" ]; then
+    echo "Error: offline build download directory $input_dir does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+echo "Find ${nvcc_file}"
+
+if [ ! -f "$cuobjdump_file" ]; then
+    echo "Error: File $cuobjdump_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+echo "Find ${cuobjdump_file}"
+
+if [ ! -f "$nvdisam_file" ]; then
+    echo "Error: File $nvdisam_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+echo "Find ${nvdisam_file}"
+
+if [ ! -f "$cudart_file" ]; then
+    echo "Error: File $cudart_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+echo "Find ${cudart_file}"
+
+if [ ! -f "$cupti_file" ]; then
+    echo "Error: File $cupti_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+echo "Find ${cupti_file}"
+
+if [ ! -f "$json_file" ]; then
+    echo "Error: File $json_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+
+if [ ! -f "$pybind11_file" ]; then
+    echo "Error: File $pybind11_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+
+if [ ! -f "$googletest_file" ]; then
+    echo "Error: File $googletest_file does not exist, run README_offline_build.sh for more information"
+    exit 1
+fi
+
+if [ ! -f "$triton_shared_file" ]; then
+    echo "Warning: File $triton_shared_file does not exist. This file is optional, please check if you need it."
+    triton_shared_file=""
+fi
+
+echo "Compressing..."
+zip "$output_zip" "$nvcc_file" "$cuobjdump_file" "$nvdisam_file" "$cudart_file" "$cupti_file" \
+    "$json_file" "$pybind11_file" "$googletest_file" "$triton_shared_file"
+
+echo ""
+if [ $? -eq 0 ]; then
+    echo "Offline Build dependencies are successfully compressed into $output_zip"
+    exit 0
+else
+    echo "Error: Failed to compress offline build dependencies"
+    exit 1
+fi
