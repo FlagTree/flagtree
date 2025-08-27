bc8df075d4103b28b531ad199ba24e5ef7209f65
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-08-27 14:48:18 +0800

    [Build] Optimized offline build scripts

--- utils/offline_build_pack.sh
+++ utils/offline_build_pack.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo " =================== Start Packing Downloaded Offline Build Files ==================="
-echo ""
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e " =================== Start Packing Downloaded Offline Build Files ==================="
+echo -e ""
 # detect pybind11 version requirement
 PYBIND11_VERSION_FILE="cmake/pybind11-version.txt"
 if [ -f "$PYBIND11_VERSION_FILE" ]; then
     pybind11_version=$(tr -d '\n' < "$PYBIND11_VERSION_FILE")
-    echo "Pybind11 Version Required: $pybind11_version"
+    echo -e "Pybind11 Version Required: $pybind11_version"
 else
-    echo "Error: version file $PYBIND11_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $PYBIND11_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
@@ -16,43 +22,40 @@ fi
 NV_TOOLCHAIN_VERSION_FILE="cmake/nvidia-toolchain-version.txt"
 if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
     nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
-    echo "Nvidia Toolchain Version Required: $nv_toolchain_version"
+    echo -e "Nvidia Toolchain Version Required: $nv_toolchain_version"
 else
-    echo "Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist${NC}"
     exit 1
 fi
+
+output_zip="offline-packed-nv${nv_toolchain_version}-pybind${pybind11_version}.zip"
 
 # handle input
-echo ""
+echo -e ""
 if [ $# -ge 1 ]; then
     input_dir="$1"
+    echo -e "${BLUE}Use $input_dir as input directory${NC}"
 else
-    echo "Using default input directory: $HOME/.flagtree-offline-download"
-    input_dir="$HOME/.flagtree-offline-download"
-fi
-
-if [ ! -d "$input_dir" ]; then
-    echo "Error: Cannot find input directory $input_dir"
+    echo -e "${RED}Error: No input directory specified${NC}"
+    echo -e "${GREEN}Usage: sh utils/offline_build_pack.sh [input_dir] [output_zip_file]${NC}"
     exit 1
-else
-    echo "Find input directory: $input_dir"
 fi
-echo ""
 
 # handle output
 if [ $# -ge 2 ]; then
-    output_dir="$2"
+    output_zip="$2"
+    echo -e "${BLUE}Use $output_zip as output .zip file${NC}"
 else
-    output_dir="$PWD"
+    echo -e "${YELLOW}Use default output .zip file name: $output_zip${NC}"
 fi
 
-if [ ! -d "$output_dir" ]; then
-    echo "Creating output directory $output_dir"
-    mkdir -p "$output_dir"
+if [ ! -d "$input_dir" ]; then
+    echo -e "${RED}Error: Cannot find input directory $input_dir${NC}"
+    exit 1
 else
-    echo "Output directory $output_dir already exists"
+    echo -e "Find input directory: $input_dir"
 fi
-echo ""
+echo -e ""
 
 nvcc_file="cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
 cuobjdump_file="cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
@@ -64,78 +67,76 @@ pybind11_file="pybind11-${pybind11_version}.tar.gz"
 googletest_file="googletest-release-1.12.1.zip"
 triton_shared_file="triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
 
-output_zip="offline-packed-nv${nv_toolchain_version}-pybind${pybind11_version}.zip"
-
 if [ ! -d "$input_dir" ]; then
-    echo "Error: offline build download directory $input_dir does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: offline build download directory $input_dir does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find ${nvcc_file}"
+echo -e "Find ${nvcc_file}"
 
 if [ ! -f "$input_dir/$cuobjdump_file" ]; then
-    echo "Error: File $input_dir/$cuobjdump_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$cuobjdump_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$cuobjdump_file"
+echo -e "Find $input_dir/$cuobjdump_file"
 
 if [ ! -f "$input_dir/$nvdisam_file" ]; then
-    echo "Error: File $input_dir/$nvdisam_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$nvdisam_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$nvdisam_file"
+echo -e "Find $input_dir/$nvdisam_file"
 
 if [ ! -f "$input_dir/$cudart_file" ]; then
-    echo "Error: File $input_dir/$cudart_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$cudart_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$cudart_file"
+echo -e "Find $input_dir/$cudart_file"
 
 if [ ! -f "$input_dir/$cupti_file" ]; then
-    echo "Error: File $input_dir/$cupti_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$cupti_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$cupti_file"
+echo -e "Find $input_dir/$cupti_file"
 
 if [ ! -f "$input_dir/$json_file" ]; then
-    echo "Error: File $input_dir/$json_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$json_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$json_file"
+echo -e "Find $input_dir/$json_file"
 
 if [ ! -f "$input_dir/$pybind11_file" ]; then
-    echo "Error: File $input_dir/$pybind11_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$pybind11_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$pybind11_file"
+echo -e "Find $input_dir/$pybind11_file"
 
 if [ ! -f "$input_dir/$googletest_file" ]; then
-    echo "Error: File $input_dir/$googletest_file does not exist, run README_offline_build.sh for more information"
+    echo -e "${RED}Error: File $input_dir/$googletest_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo "Find $input_dir/$googletest_file"
+echo -e "Find $input_dir/$googletest_file"
 
 if [ ! -f "$input_dir/$triton_shared_file" ]; then
-    echo "Warning: File $input_dir/$triton_shared_file does not exist. This file is optional, please check if you need it."
+    echo -e "Warning: File $input_dir/$triton_shared_file does not exist. This file is optional, please check if you need it."
     triton_shared_file=""
 else
-    echo "Find $input_dir/$triton_shared_file"
+    echo -e "Find $input_dir/$triton_shared_file"
 fi
 
-echo "cd ${input_dir}"
+echo -e "cd ${input_dir}"
 cd "$input_dir"
 
-echo "Compressing..."
-zip "$output_dir/$output_zip" "$nvcc_file" "$cuobjdump_file" "$nvdisam_file" "$cudart_file" "$cupti_file" \
+echo -e "Compressing..."
+zip "$output_zip" "$nvcc_file" "$cuobjdump_file" "$nvdisam_file" "$cudart_file" "$cupti_file" \
     "$json_file" "$pybind11_file" "$googletest_file" "$triton_shared_file"
 
-echo "cd -"
+echo -e "cd -"
 cd -
 
-echo ""
+echo -e ""
 if [ $? -eq 0 ]; then
-    echo "Offline Build dependencies are successfully compressed into $output_dir/$output_zip"
+    echo -e "${GREEN}Offline Build dependencies are successfully compressed into $output_zip${NC}"
     exit 0
 else
-    echo "Error: Failed to compress offline build dependencies"
+    echo -e "${RED}Error: Failed to compress offline build dependencies${NC}"
     exit 1
 fi
