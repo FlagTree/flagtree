b17b164b963788d115aaafaa1abfa833927594f8
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-08-27 14:48:18 +0800

    [Build] Optimized offline build scripts

--- utils/offline_build_download.sh
+++ utils/offline_build_download.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-echo " =================== Start Downloading Offline Build Files ==================="
+echo -e " =================== Start Downloading Offline Build Files ==================="
 # detect pybind11 version requirement
 PYBIND11_VERSION_FILE="cmake/pybind11-version.txt"
 if [ -f "$PYBIND11_VERSION_FILE" ]; then
     pybind11_version=$(tr -d '\n' < "$PYBIND11_VERSION_FILE")
-    echo "Pybind11 Version Required: $pybind11_version"
+    echo -e "Pybind11 Version Required: $pybind11_version"
 else
-    echo "Error: version file $PYBIND11_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $PYBIND11_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
@@ -16,15 +21,28 @@ fi
 NV_TOOLCHAIN_VERSION_FILE="cmake/nvidia-toolchain-version.txt"
 if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
     nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
-    echo "Nvidia Toolchain Version Required: $nv_toolchain_version"
+    echo -e "Nvidia Toolchain Version Required: $nv_toolchain_version"
 else
-    echo "Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
-# detect system arch
-arch=$(uname -m)
-echo "Detected the system arch: $arch"
+# handle system arch
+if [ $# -eq 0 ]; then
+    echo -e "${RED}Error: No system architecture specified for offline build.${NC}"
+    echo -e "${GREEN}Usage: sh $0 arch=<system arch>${NC}"
+    echo -e "You need to specify the target system architecture to build the FlagTree"
+    echo -e "Supported system arch values: ${GREEN}x86_64, arm64, aarch64${NC}"
+    exit 1
+fi
+
+arch_param="$1"
+if [[ "$arch_param" == arch=* ]]; then
+    arch="${arch_param#arch=}"
+else
+    arch="$arch_param"
+fi
+
 case "$arch" in
     x86_64)
         arch="64"
@@ -33,86 +51,95 @@ case "$arch" in
         arch="aarch64"
         ;;
     *)
+        echo -e "${RED}Error: Unsupported system architecture '$arch'.${NC}"
+        echo -e "${GREEN}Usage: sh $0 arch=<system arch> <output_dir>${NC}"
+        echo -e "Supported system arch values: ${GREEN}x86_64, arm64, aarch64${NC}"
+        exit 1
         ;;
 esac
+echo -e "Target System Arch for offline building: $arch"
 
 check_download() {
     if [ $? -eq 0 ]; then
-        echo "Download Success"
+        echo -e "${GREEN}Download Success${NC}"
     else
-        echo "Download Failed !!!"
+        echo -e "${RED}Download Failed !!!${NC}"
         exit 1
     fi
-    echo ""
+    echo -e ""
 }
 
-if [ $# -ge 1 ]; then
-    target_dir="$1"
+if [ $# -ge 2 ]; then
+    target_dir="$2"
+    echo -e "Use $target_dir as download output directory"
 else
-    target_dir="$HOME/.flagtree-offline-download"
+    echo -e "${RED}Error: No output directory specified for downloading.${NC}"
+    echo -e "${GREEN}Usage: sh utils/offline_build_download.sh arch=<system arch> <output_dir>${NC}"
+    echo -e "   Support system arch values: ${GREEN}x86_64, arm64, aarch64${NC}"
+    exit 1
 fi
 
-echo ""
+echo -e ""
 if [ ! -d "$target_dir" ]; then
-    echo "Creating download output directory $target_dir"
+    echo -e "Creating download output directory $target_dir"
     mkdir -p "$target_dir"
 else
-    echo "Download output directory $target_dir already exists"
+    echo -e "Download output directory $target_dir already exists"
 fi
-echo ""
+echo -e ""
 
 nvcc_url=https://anaconda.org/nvidia/cuda-nvcc/${nv_toolchain_version}/download/linux-${arch}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2
-echo "Downloading NVCC from: $nvcc_url"
-echo "wget $nvcc_url -O ${target_dir}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
+echo -e "Downloading NVCC from: ${BLUE}$nvcc_url${NC}"
+echo -e "wget $nvcc_url -O ${target_dir}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
 wget "$nvcc_url" -O ${target_dir}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2
 check_download
 
 cuobjdump_url=https://anaconda.org/nvidia/cuda-cuobjdump/${nv_toolchain_version}/download/linux-${arch}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2
-echo "Downloading CUOBJBDUMP from: $cuobjdump_url"
-echo "wget $cuobjdump_url -O ${target_dir}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
+echo -e "Downloading CUOBJBDUMP from: ${BLUE}$cuobjdump_url${NC}"
+echo -e "wget $cuobjdump_url -O ${target_dir}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
 wget "$cuobjdump_url" -O ${target_dir}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2
 check_download
 
 nvdisam_url=https://anaconda.org/nvidia/cuda-nvdisasm/${nv_toolchain_version}/download/linux-${arch}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2
-echo "Downloading NVDISAM from: $nvdisam_url"
-echo "wget $nvdisam_url -O ${target_dir}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
+echo -e "Downloading NVDISAM from: ${BLUE}$nvdisam_url${NC}"
+echo -e "wget $nvdisam_url -O ${target_dir}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
 wget "$nvdisam_url" -O ${target_dir}/cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2
 check_download
 
 cudart_url=https://anaconda.org/nvidia/cuda-cudart-dev/${nv_toolchain_version}/download/linux-${arch}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2
-echo "Downloading CUDART from: $cudart_url"
-echo "wget $cudart_url -O ${target_dir}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
+echo -e "Downloading CUDART from: ${BLUE}$cudart_url${NC}"
+echo -e "wget $cudart_url -O ${target_dir}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
 wget "$cudart_url" -O ${target_dir}/cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2
 check_download
 
 cupti_url=https://anaconda.org/nvidia/cuda-cupti/${nv_toolchain_version}/download/linux-${arch}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2
-echo "Downloading CUPTI from: $cupti_url"
-echo "wget $cupti_url -O ${target_dir}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
+echo -e "Downloading CUPTI from: ${BLUE}$cupti_url${NC}"
+echo -e "wget $cupti_url -O ${target_dir}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
 wget "$cupti_url" -O ${target_dir}/cuda-cupti-${nv_toolchain_version}-0.tar.bz2
 check_download
 
 pybind11_url=https://github.com/pybind/pybind11/archive/refs/tags/v${pybind11_version}.tar.gz
-echo "Downloading Pybind11 from: $pybind11_url"
-echo "wget $pybind11_url -O ${target_dir}/pybind11-${pybind11_version}.tar.gz"
+echo -e "Downloading Pybind11 from: ${BLUE}$pybind11_url${NC}"
+echo -e "wget $pybind11_url -O ${target_dir}/pybind11-${pybind11_version}.tar.gz"
 wget "$pybind11_url" -O ${target_dir}/pybind11-${pybind11_version}.tar.gz
 check_download
 
 json_url=https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip
-echo "Downloading JSON library from: $json_url"
-echo "wget $json_url -O ${target_dir}/include.zip"
+echo -e "Downloading JSON library from: ${BLUE}$json_url${NC}"
+echo -e "wget $json_url -O ${target_dir}/include.zip"
 wget "$json_url" -O ${target_dir}/include.zip
 check_download
 
 googletest_url=https://github.com/google/googletest/archive/refs/tags/release-1.12.1.zip
-echo "Downloading GoogleTest from: $googletest_url"
-echo "wget $googletest_url -O ${target_dir}/googletest-release-1.12.1.zip"
+echo -e "Downloading GoogleTest from: ${BLUE}$googletest_url${NC}"
+echo -e "wget $googletest_url -O ${target_dir}/googletest-release-1.12.1.zip"
 wget "$googletest_url" -O ${target_dir}/googletest-release-1.12.1.zip
 check_download
 
 triton_shared_url=https://github.com/microsoft/triton-shared/archive/380b87122c88af131530903a702d5318ec59bb33.zip
-echo "Downloading Triton_Shared from: $triton_shared_url"
-echo "wget $triton_shared_url -O ${target_dir}/triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
+echo -e "Downloading Triton_Shared from: ${BLUE}$triton_shared_url${NC}"
+echo -e "wget $triton_shared_url -O ${target_dir}/triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
 wget "$triton_shared_url" -O ${target_dir}/triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip
 check_download
 
-echo " =================== Done ==================="
+echo -e " =================== Done ==================="
--- utils/offline_build_unpack.sh
+++ utils/offline_build_unpack.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo " =================== Start Preparing Offline Build Dependencies ==================="
-echo ""
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e " =================== Start Unpacking Offline Build Dependencies ==================="
+echo -e ""
 # detect pybind11 version requirement
 PYBIND11_VERSION_FILE="cmake/pybind11-version.txt"
 if [ -f "$PYBIND11_VERSION_FILE" ]; then
     pybind11_version=$(tr -d '\n' < "$PYBIND11_VERSION_FILE")
-    echo "Pybind11 Version Required: $pybind11_version"
+    echo -e "Pybind11 Version Required: $pybind11_version"
 else
-    echo "Error: version file $PYBIND11_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $PYBIND11_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
@@ -16,53 +22,39 @@ fi
 NV_TOOLCHAIN_VERSION_FILE="cmake/nvidia-toolchain-version.txt"
 if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
     nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
-    echo "Nvidia Toolchain Version Required: $nv_toolchain_version"
+    echo -e "Nvidia Toolchain Version Required: $nv_toolchain_version"
 else
-    echo "Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist"
+    echo -e "${RED}Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
-zip_file="offline-packed-nv${nv_toolchain_version}-pybind${pybind11_version}.zip"
-
-# handle input
-echo ""
-if [ $# -ge 1 ]; then
-    input_dir="$1"
-else
-    echo "Using default input directory: $PWD"
-    input_dir="$PWD"
-fi
-
-if [ ! -d "$input_dir" ]; then
-    echo "Error: Cannot find input directory: $input_dir"
-    exit 1
-else
-    echo "Find input directory: $input_dir"
-fi
-
-if [ ! -f "${input_dir}/${zip_file}" ]; then
-    echo "Error: Cannot find $zip_file in directory: $input_dir"
-    exit 1
-else
-    echo "Find input packed .zip file: ${input_dir}/${zip_file}"
-fi
-echo ""
-
-# handle output
+# handle params
 if [ $# -ge 2 ]; then
+    input_zip="$1"
     output_dir="$2"
+    echo -e "${BLUE}Use $input_zip as input packed .zip file${NC}"
+    echo -e "${BLUE}Use $output_dir as output directory${NC}"
 else
-    echo "Using default output directory: $HOME/.flagtree-offline-build"
-    output_dir="$HOME/.flagtree-offline-build"
+    echo -e "${RED}Error: No input file or output directory specified${NC}"
+    echo -e "${GREEN}Usage: sh utils/offline_build_unpack.sh [input_zip] [output_dir]${NC}"
+    exit 1
 fi
+
+if [ ! -f "${input_zip}" ]; then
+    echo -e "${RED}Error: Cannot find input file $input_zip${NC}"
+    exit 1
+else
+    echo -e "Find input packed .zip file: ${input_zip}"
+fi
+echo -e ""
 
 if [ ! -d "$output_dir" ]; then
-    echo "Creating output directory $output_dir"
+    echo -e "Creating output directory $output_dir"
     mkdir -p "$output_dir"
 else
-    echo "Output directory $output_dir already exists"
+    echo -e "Output directory $output_dir already exists"
 fi
-echo ""
+echo -e ""
 
 nvcc_file="${output_dir}/cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
 cuobjdump_file="${output_dir}/cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
@@ -82,83 +74,83 @@ fi
 
 
 
-echo "Unpacking $input_dir/$zip_file into ${output_dir}..."
-unzip "${input_dir}/${zip_file}" -d ${output_dir}
+echo -e "Unpacking ${input_zip} into ${output_dir}..."
+unzip "${input_zip}" -d ${output_dir}
 
-echo "Creating directory ${output_dir}/nvidia..."
+echo -e "Creating directory ${output_dir}/nvidia ..."
 mkdir "${output_dir}/nvidia"
 
-echo "Creating directory ${output_dir}/nvidia/ptxas ..."
+echo -e "Creating directory ${output_dir}/nvidia/ptxas ..."
 mkdir "${output_dir}/nvidia/ptxas"
-echo "Extracting $nvcc_file into ${output_dir}/nvidia/ptxas ..."
+echo -e "Extracting $nvcc_file into ${output_dir}/nvidia/ptxas ..."
 tar -jxf $nvcc_file -C "${output_dir}/nvidia/ptxas"
 
-echo "Creating directory ${output_dir}/nvidia/cuobjdump ..."
+echo -e "Creating directory ${output_dir}/nvidia/cuobjdump ..."
 mkdir "${output_dir}/nvidia/cuobjdump"
-echo "Extracting $cuobjdump_file into ${output_dir}/nvidia/cuobjdump ..."
+echo -e "Extracting $cuobjdump_file into ${output_dir}/nvidia/cuobjdump ..."
 tar -jxf $cuobjdump_file -C "${output_dir}/nvidia/cuobjdump"
 
-echo "Creating directory ${output_dir}/nvidia/nvdisasm ..."
+echo -e "Creating directory ${output_dir}/nvidia/nvdisasm ..."
 mkdir "${output_dir}/nvidia/nvdisasm"
-echo "Extracting $nvdisam_file into ${output_dir}/nvidia/nvdisasm ..."
+echo -e "Extracting $nvdisam_file into ${output_dir}/nvidia/nvdisasm ..."
 tar -jxf $nvdisam_file -C "${output_dir}/nvidia/nvdisasm"
 
-echo "Creating directory ${output_dir}/nvidia/cudacrt ..."
+echo -e "Creating directory ${output_dir}/nvidia/cudacrt ..."
 mkdir "${output_dir}/nvidia/cudacrt"
-echo "Extracting $nvcc_file into ${output_dir}/nvidia/cudacrt ..."
+echo -e "Extracting $nvcc_file into ${output_dir}/nvidia/cudacrt ..."
 tar -jxf $nvcc_file -C "${output_dir}/nvidia/cudacrt"
 
-echo "Creating directory ${output_dir}/nvidia/cudart ..."
+echo -e "Creating directory ${output_dir}/nvidia/cudart ..."
 mkdir "${output_dir}/nvidia/cudart"
-echo "Extracting $cudart_file into ${output_dir}/nvidia/cudart ..."
+echo -e "Extracting $cudart_file into ${output_dir}/nvidia/cudart ..."
 tar -jxf $cudart_file -C "${output_dir}/nvidia/cudart"
 
-echo "Creating directory ${output_dir}/nvidia/cupti ..."
+echo -e "Creating directory ${output_dir}/nvidia/cupti ..."
 mkdir "${output_dir}/nvidia/cupti"
-echo "Extracting $cupti_file into ${output_dir}/nvidia/cupti ..."
+echo -e "Extracting $cupti_file into ${output_dir}/nvidia/cupti ..."
 tar -jxf $cupti_file -C "${output_dir}/nvidia/cupti"
 
-echo "Creating directory ${output_dir}/json ..."
+echo -e "Creating directory ${output_dir}/json ..."
 mkdir "${output_dir}/json"
-echo "Extracting $json_file into ${output_dir}/json ..."
+echo -e "Extracting $json_file into ${output_dir}/json ..."
 unzip $json_file -d "${output_dir}/json" > /dev/null
 
-echo "Creating directory ${output_dir}/pybind11 ..."
+echo -e "Creating directory ${output_dir}/pybind11 ..."
 mkdir "${output_dir}/pybind11"
-echo "Extracting $pybind11_file into ${output_dir}/pybind11 ..."
+echo -e "Extracting $pybind11_file into ${output_dir}/pybind11 ..."
 tar -zxf $pybind11_file -C "${output_dir}/pybind11"
 
-echo "Extracting $googletest_file into ${output_dir}/googletest-release-1.12.1 ..."
+echo -e "Extracting $googletest_file into ${output_dir}/googletest-release-1.12.1 ..."
 unzip $googletest_file -d "${output_dir}" > /dev/null
 
 if [ -f "${triton_shared_file}" ]; then
-    echo "Extracting $triton_shared_file into ${output_dir}/triton_shared ..."
+    echo -e "Extracting $triton_shared_file into ${output_dir}/triton_shared ..."
     unzip $triton_shared_file -d "${output_dir}" > /dev/null
     mv ${output_dir}/triton-shared-380b87122c88af131530903a702d5318ec59bb33 ${output_dir}/triton_shared
 else
-    echo "Warning: File $triton_shared_file does not exist. This file is optional, please check if you need it."
+    echo -e "Warning: File $triton_shared_file does not exist. This file is optional, please check if you need it."
 fi
 
-echo ""
-echo "Delete $nvcc_file"
+echo -e ""
+echo -e "Delete $nvcc_file"
 rm $nvcc_file
-echo "Delete $cuobjdump_file"
+echo -e "Delete $cuobjdump_file"
 rm $cuobjdump_file
-echo "Delete $nvdisam_file"
+echo -e "Delete $nvdisam_file"
 rm $nvdisam_file
-echo "Delete $cudart_file"
+echo -e "Delete $cudart_file"
 rm $cudart_file
-echo "Delete $cupti_file"
+echo -e "Delete $cupti_file"
 rm $cupti_file
-echo "Delete $json_file"
+echo -e "Delete $json_file"
 rm $json_file
-echo "Delete $pybind11_file"
+echo -e "Delete $pybind11_file"
 rm $pybind11_file
-echo "Delete $googletest_file"
+echo -e "Delete $googletest_file"
 rm $googletest_file
 if [ -f "${triton_shared_file}" ]; then
-    echo "Delete $triton_shared_file"
+    echo -e "Delete $triton_shared_file"
     rm $triton_shared_file
 fi
-echo "Delete useless file: ${output_dir}/nvidia/cudart/lib/libcudart.so"
+echo -e "Delete useless file: ${output_dir}/nvidia/cudart/lib/libcudart.so"
 rm ${output_dir}/nvidia/cudart/lib/libcudart.so
