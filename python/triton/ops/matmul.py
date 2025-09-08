540db518564f4d6cdbfa389ad552d9c8fda5d900
zhengyang <zhengyang@baai.ac.cn>
2025-09-08 17:20:44 +0800

    [Decoupling] iluvatar python triton ops

--- python/triton/ops/matmul.py
+++ python/triton/ops/matmul.py
@@ -208,15 +208,15 @@ class _matmul(torch.autograd.Function):
         # flagtree backend specialization
         from triton.runtime.driver import flagtree_backend_specialization
         flagtree_backend_specialization("matmul_kernel", grid, a, b, c, M, N, K, acc_dtype, input_precision, fp8_fast_accum, ab_dtype) or \
-            _kernel[grid](
-                a, b, c, M, N, K,  #
-                a.stride(0), a.stride(1),  #
-                b.stride(0), b.stride(1),  #
-                c.stride(0), c.stride(1),  #
-                acc_dtype=acc_dtype,  #
-                input_precision=input_precision,  #
-                fp8_fast_accum=fp8_fast_accum,  #
-                GROUP_M=8, AB_DTYPE=ab_dtype)
+        _kernel[grid](
+            a, b, c, M, N, K,  #
+            a.stride(0), a.stride(1),  #
+            b.stride(0), b.stride(1),  #
+            c.stride(0), c.stride(1),  #
+            acc_dtype=acc_dtype,  #
+            input_precision=input_precision,  #
+            fp8_fast_accum=fp8_fast_accum,  #
+            GROUP_M=8, AB_DTYPE=ab_dtype)
         return c
 
     @staticmethod
