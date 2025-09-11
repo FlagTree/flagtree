1fe98c185fc70ac858c3da1e82acdab3ea981470
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-09-11 16:08:39 +0800

    [Build] Fix BUGs in offline build scripts for Triton-v3.3.x

--- python/scripts/offline_build_pack.sh
+++ python/scripts/offline_build_pack.sh
@@ -10,16 +10,29 @@ echo -e " =================== Start Packing Downloaded Offline Build Files =====
 echo -e ""
 
 # detect nvidia toolchain version requirement
-NV_TOOLCHAIN_VERSION_FILE="../cmake/nvidia-toolchain-version.txt"
+NV_TOOLCHAIN_VERSION_FILE="../cmake/nvidia-toolchain-version.json"
 if [ -f "$NV_TOOLCHAIN_VERSION_FILE" ]; then
-    nv_toolchain_version=$(tr -d '\n' < "$NV_TOOLCHAIN_VERSION_FILE")
-    echo -e "Nvidia Toolchain Version Required: $nv_toolchain_version"
+    ptxas_blackwell_version=$(grep '"ptxas-blackwell"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"ptxas-blackwell": "([^"]+)".*/\1/')
+    ptxas_version=$(grep '"ptxas"' "$NV_TOOLCHAIN_VERSION_FILE" | grep -v "ptxas-blackwell" | sed -E 's/.*"ptxas": "([^"]+)".*/\1/')
+    cuobjdump_version=$(grep '"cuobjdump"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"cuobjdump": "([^"]+)".*/\1/')
+    nvdisasm_version=$(grep '"nvdisasm"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"nvdisasm": "([^"]+)".*/\1/')
+    cudacrt_version=$(grep '"cudacrt"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"cudacrt": "([^"]+)".*/\1/')
+    cudart_version=$(grep '"cudart"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"cudart": "([^"]+)".*/\1/')
+    cupti_version=$(grep '"cupti"' "$NV_TOOLCHAIN_VERSION_FILE" | sed -E 's/.*"cupti": "([^"]+)".*/\1/')
+    echo -e "Nvidia Toolchain Version Requirement:"
+    echo -e "   ptxas: $ptxas_version"
+    echo -e "   ptxas-blackwell: $ptxas_blackwell_version"
+    echo -e "   cuobjdump: $cuobjdump_version"
+    echo -e "   nvdisasm: $nvdisasm_version"
+    echo -e "   cudacrt: $cudacrt_version"
+    echo -e "   cudart: $cudart_version"
+    echo -e "   cupti: $cupti_version"
 else
     echo -e "${RED}Error: version file $NV_TOOLCHAIN_VERSION_FILE is not exist${NC}"
     exit 1
 fi
 
-output_zip="offline-packed-nv${nv_toolchain_version}.zip"
+output_zip="offline-build-pack-triton-3.3.x.zip"
 
 # handle input
 echo -e ""
@@ -48,14 +61,16 @@ else
 fi
 echo -e ""
 
-nvcc_file="cuda-nvcc-${nv_toolchain_version}-0.tar.bz2"
-cuobjdump_file="cuda-cuobjdump-${nv_toolchain_version}-0.tar.bz2"
-nvdisam_file="cuda-nvdisasm-${nv_toolchain_version}-0.tar.bz2"
-cudart_file="cuda-cudart-dev-${nv_toolchain_version}-0.tar.bz2"
-cupti_file="cuda-cupti-${nv_toolchain_version}-0.tar.bz2"
+nvcc_ptxas_file="cuda-nvcc-${ptxas_version}.tar.xz"
+nvcc_ptxas_blackwell_file="cuda-nvcc-${ptxas_blackwell_version}.tar.xz"
+nvcc_cudacrt_file="cuda-nvcc-${cudacrt_version}.tar.xz"
+cuobjdump_file="cuda-cuobjdump-${cuobjdump_version}.tar.xz"
+nvdisasm_file="cuda-nvdisasm-${nvdisasm_version}.tar.xz"
+cudart_file="cuda-cudart-dev-${cudart_version}.tar.xz"
+cupti_file="cuda-cupti-${cupti_version}.tar.xz"
 json_file="include.zip"
 googletest_file="googletest-release-1.12.1.zip"
-triton_shared_file="triton-shared-380b87122c88af131530903a702d5318ec59bb33.zip"
+triton_shared_file="triton-shared-5842469a16b261e45a2c67fbfc308057622b03ee.zip"
 
 if [ ! -d "$input_dir" ]; then
     echo -e "${RED}Error: offline build download directory $input_dir does not exist, run README_offline_build.sh for more information${NC}"
@@ -69,11 +84,11 @@ if [ ! -f "$input_dir/$cuobjdump_file" ]; then
 fi
 echo -e "Find $input_dir/$cuobjdump_file"
 
-if [ ! -f "$input_dir/$nvdisam_file" ]; then
-    echo -e "${RED}Error: File $input_dir/$nvdisam_file does not exist, run README_offline_build.sh for more information${NC}"
+if [ ! -f "$input_dir/$nvdisasm_file" ]; then
+    echo -e "${RED}Error: File $input_dir/$nvdisasm_file does not exist, run README_offline_build.sh for more information${NC}"
     exit 1
 fi
-echo -e "Find $input_dir/$nvdisam_file"
+echo -e "Find $input_dir/$nvdisasm_file"
 
 if [ ! -f "$input_dir/$cudart_file" ]; then
     echo -e "${RED}Error: File $input_dir/$cudart_file does not exist, run README_offline_build.sh for more information${NC}"
@@ -100,7 +115,7 @@ fi
 echo -e "Find $input_dir/$googletest_file"
 
 if [ ! -f "$input_dir/$triton_shared_file" ]; then
-    echo -e "Warning: File $input_dir/$triton_shared_file does not exist. This file is optional, please check if you need it."
+    echo -e "${YELLOW}Warning: File $input_dir/$triton_shared_file does not exist. This file is optional, please check if you need it.${NC}"
     triton_shared_file=""
 else
     echo -e "Find $input_dir/$triton_shared_file"
@@ -110,8 +125,8 @@ echo -e "cd ${input_dir}"
 cd "$input_dir"
 
 echo -e "Compressing..."
-zip "$output_zip" "$nvcc_file" "$cuobjdump_file" "$nvdisam_file" "$cudart_file" "$cupti_file" \
-    "$json_file" "$googletest_file" "$triton_shared_file"
+zip "$output_zip" "$nvcc_ptxas_file" "$nvcc_cudacrt_file" "$nvcc_ptxas_blackwell_file" "$cuobjdump_file" "$nvdisasm_file" "$cudart_file" \
+    "$cupti_file" "$json_file" "$googletest_file" "$triton_shared_file"
 
 echo -e "cd -"
 cd -
