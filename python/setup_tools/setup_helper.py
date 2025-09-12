d4500547bf2140c2b706e663257ff1fe44b6c5dd
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-09-12 14:21:54 +0800

    Fix download flagtree third party print info

--- python/setup_tools/setup_helper.py
+++ python/setup_tools/setup_helper.py
@@ -85,7 +85,7 @@ def download_flagtree_third_party(name, condition, required=False, hock=None):
         submoduel = utils.flagtree_submoduels[name]
         succ = utils.download_module(submoduel, required)
         if not succ:
-            print('[INFO] Download/Copy triton_shared failed. Skip appending triton_shared as default backend')
+            print(f'[INFO] Download/Copy {name} failed.')
         if callable(hock) and succ:
             hock(third_party_base_dir=utils.flagtree_submoduel_dir, backend=submoduel,
                  default_backends=default_backends)
