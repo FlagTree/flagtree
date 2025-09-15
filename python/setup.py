416d8de59362bb5fb45a342fb599f80f066c5938
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-09-15 11:53:54 +0800

    Fix: pre-commit formatter check

--- python/setup.py
+++ python/setup.py
@@ -383,8 +383,7 @@ class CMakeBuild(build_ext):
             "-DTRITON_PLUGIN_DIRS=" + ';'.join([b.src_dir for b in backends if b.is_external])
         ]
         if offline_build:
-            googletest_offline_path = os.path.join(offline_handler.offline_build_dir,
-                                                   "googletest-release-1.12.1")
+            googletest_offline_path = os.path.join(offline_handler.offline_build_dir, "googletest-release-1.12.1")
             print(f'[INFO] Offline Build: Using offline googletest from {googletest_offline_path}')
             cmake_args += ["-DGOOGLETEST_DIR=" + googletest_offline_path]
         helper.get_backend_cmake_args(build_ext=self)
