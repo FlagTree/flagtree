82f037a085b209d171de0fdbf7f59e01768ccd45
jinjieliu <jjliu@baai.ac.cn>
2025-09-18 02:07:53 +0000

    refactor UnifiedHardware
    Signed-off-by: jinjieliu <jjliu@baai.ac.cn>

--- third_party/aipu/lib/Registrar.cc
+++ third_party/aipu/lib/Registrar.cc
@@ -1,2 +1,13 @@
 #include "flagtree/Common/UnifiedHardware.h"
-FLAGTREE_REGISTRAR(DMATag, 11)
+
+class AipuUnifiedHardware : public mlir::flagtree::UnifiedHardware {
+public:
+  int getDMATag() override;
+};
+
+int AipuUnifiedHardware::getDMATag() { return 11; }
+
+std::unique_ptr<mlir::flagtree::UnifiedHardware>
+mlir::flagtree::createUnifiedHardwareManager() {
+  return std::make_unique<AipuUnifiedHardware>();
+}
