aa8f34224ef90fec787b3d1179b1aaf9e00ab63c
SabYic <sunwenjia04@163.com>
2025-09-24 18:40:56 +0800

    modified:   bin/CMakeLists.txt 	modified:   python/setup.py

--- python/setup.py
+++ python/setup.py
@@ -428,7 +428,10 @@ class CMakeBuild(build_ext):
 
         if helper.flagtree_backend == "iluvatar":
             src_plugin_path = str(os.getenv("HOME")) + "/.flagtree/iluvatar/iluvatarTritonPlugin.so"
-            dst_plugin_path = sysconfig.get_paths()['purelib'] + "/triton/_C/iluvatarTritonPlugin.so"
+            dst_plugin_dir = sysconfig.get_paths()['purelib'] + "/triton/_C"
+            if not os.path.exists(dst_plugin_dir):
+               os.makedirs(dst_plugin_dir)
+            dst_plugin_path = dst_plugin_dir+"/iluvatarTritonPlugin.so"
             shutil.copy(src_plugin_path, dst_plugin_path)
 
         env = os.environ.copy()
