66688537009c9306c8c475ffa09341f6380b2dfa
wjsun <sunwenjia04@163.com>
2025-09-25 16:13:22 +0800

    修改：     CMakeLists.txt 	修改：     python/src/plugin.h

--- python/src/plugin.h
+++ python/src/plugin.h
@@ -8,6 +8,7 @@
 #include <optional>
 #include <string>
 #include <string_view>
+#include<filesystem>
 
 #define DEFINE_LOAD_FUNC(symbol_name)                                          \
   static symbol_name##Func load_##symbol_name##_func(const char *backend_name, \
@@ -32,36 +33,40 @@ static std::optional<std::string> get_env(std::string_view key) {
 }
 
 static void *load_backend_plugin(const char *backend_name) {
-  const std::string lib_name = get_env("HOME").value_or("nobackend") +
-                               "/.flagtree/" + std::string(backend_name);
-  const std::string &plugin_path =
-      get_env("FLAGTREE_BACKEND_PLUGIN_LIB_DIR").value_or(DEFAULT_PLUGIN_DIR) +
-      "/" + backend_name + "TritonPlugin.so";
-  void *handle = dlopen(plugin_path.c_str(), RTLD_LAZY);
+  const std::string lib_name =  DEFAULT_PLUGIN_DIR +"/" + backend_name + "TritonPlugin.so";
+  void *handle = dlopen(lib_name.c_str(), RTLD_LAZY);
+  if (!handle) {
+    std::cerr<<"Warnings: can not find Plugin.so at default path"
+              "We will get the Plugin.so from $ENV{FLAGTREE_BACKEND_PLUGIN_LIB_DIR}"
+              " and help you copy it to default path";
+    if(auto lib_dir = get_env("FLAGTREE_BACKEND_PLUGIN_LIB_DIR")){
+      const std::string src_path=*lib_dir+"/"+backend_name+"TritonPlugin.so";
+      std::filesystem::copy_file(src_path,lib_name);
+      try
+      {
+         std::filesystem::copy_file(src_path,lib_name);
+      }
+      catch (std::filesystem::filesystem_error& e)
+      {
+        std::cout << "Could not copy the share library: " << e.what() << '\n';
+      }
+      handle=dlopen(lib_name.c_str(), RTLD_LAZY);
+    }
+  }
   if (!handle) {
     std::cerr << "Failed to load plugin: " << std::string(dlerror());
-    std::cerr << "We define your shared library path as "
-                 "$ENV{FLAGTREE_BACKEND_PLUGIN_LIB_DIR}/"
-                 "$ENV{FLAGTREE_BACKEND}TritonPlugin.so.\n"
-                 "You must set $ENV{FLAGTREE_BACKEND}. You may choose not to "
-                 "set $ENV{FLAGTREE_BACKEND_PLUGIN_LIB_DIR}; "
-                 "in that case the default choice $ENV{HOME}/.flagtree/ will "
-                 "be used.\n"
-                 "If you have not yet downloaded the shared library, please "
-                 "download it from\n"
+    std::cerr << "We could not find your shared library in the default directory, nor could we find the environment variable. "
+                 "If you haven't downloaded the shared library yet, please download it from the following URLs:\n"
                  "{\"backend\":\"iluvatar\",\"urls\":\"https://github.com/"
                  "FlagTree/flagtree/releases/download/"
                  "v0.3.0-build-deps/"
-                 "iluvatarTritonPlugin-cpython3.10-glibc2.30-glibcxx3.4.28-"
+                 "iluvatarTritonPlugin-cpython3.10-glibc2.30-glibcxx3.4.28-"c
                  "cxxabi1.3.12-ubuntu-x86_64.tar.gz\"} and\n"
                  "{\"backend\":\"mtheads\",\"urls\":\"https://github.com/"
                  "FlagTree/flagtree/releases/download/v0.3.0-build-deps/"
                  "mthreadsTritonPlugin-cpython3.10-glibc2.35-glibcxx3.4.30-"
                  "cxxabi1.3.13-ubuntu-x86_64_v0.3.0.tar.gz\"},\n"
-                 "then set $ENV{FLAGTREE_BACKEND} to the corresponding backend "
-                 "name and "
-                 "$ENV{FLAGTREE_BACKEND_PLUGIN_LIB_DIR} to the directory where "
-                 "the shared library is located.\n";
+                 "then set $ENV{FLAGTREE_BACKEND_PLUGIN_LIB_DIR} to the directory where the shared library is located.\n"
     assert(handle);
   }
   return handle;
