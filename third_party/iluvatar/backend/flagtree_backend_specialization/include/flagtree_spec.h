ffc374c39d846495ff4e1d9e5f4f72ad39f16385
toudefu <2696810816@qq.com>
2025-09-25 09:58:50 +0800

    conversion tritonGPUToLLVM TargetInfoBase decoupling

--- third_party/iluvatar/backend/flagtree_backend_specialization/include/flagtree_spec.h
+++ third_party/iluvatar/backend/flagtree_backend_specialization/include/flagtree_spec.h
@@ -2,3 +2,4 @@
 #include "triton/Analysis/iluvatar_Membar.h"
 #include "triton/Analysis/iluvatar_Utility.h"
 #include "triton/Conversion/TritonGPUToLLVM/iluvatar_ElementwiseOpToLLVMBase.h"
+#include "triton/Conversion/TritonGPUToLLVM/iluvatar_TargetInfoBase.h"
