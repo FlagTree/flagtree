9af8e7a5ce7207ff0911e9b9600be7d433175f37
liuyunqi20 <liuyunqi20@mails.ucas.ac.cn>
2025-10-21 18:07:23 +0800

    [Build] Update DownloadManager

--- python/setup_tools/utils/tools.py
+++ python/setup_tools/utils/tools.py
@@ -83,14 +83,38 @@ class DownloadManager:
         self.current_url = None
         self.current_dst_path = None
         self.current_file_name = None
+        self.module_offline_handler = OfflineBuildManager()
         NetConfig.headers = {'User-Agent': NetConfig.user_agent}
 
     def download(self, url=None, path=None, file_name=None, mode=None, module=None, required=False):
-        self.init_single_src_settings(url, path, file_name, mode)
+        if self.module_offline_handler.is_offline_build():
+            self.offline_copy(module, required)
+            return
+
+        if url:
+            self.init_single_src_settings(url, path, file_name, mode)
         if mode == "git" or module:
             return self.git_clone(module, required)
         else:
             return self.general_download(is_decompress=True)
+
+    def offline_copy(self, module, required):
+        src_path = os.path.join(self.module_offline_handler.offline_build_dir, module.name)
+        succ = os.path.exists(src_path)
+        try:
+            if succ:
+                print(f"[INFO] Offline Build: Found {module.name} at {src_path}")
+                self.module_offline_handler.src = os.path.join(self.module_offline_handler.offline_build_dir, module.name)
+                self.module_offline_handler.copy_to_flagtree_project({"dst_path": module.dst_path})
+            else:
+                print(f"[INFO] Offline Build: {module.name} is not found in offline build directory.")
+        except Exception:
+            if(required):
+                raise RuntimeError(
+                    f"[ERROR] Failed to copy {module.name} from offline build directory.")
+            print(f"[WARNING] Failed to copy {module.name} from offline build directory.")
+            pass
+
 
     def init_single_src_settings(self, url, path, file_name, mode):
         self.current_url = url
