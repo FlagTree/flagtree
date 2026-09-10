d25870dd27f26fab6d47bdf7435172d5eaf55c7a
zhzhcookie <zhengyang@baai.ac.cn>
2026-09-10 12:55:28 +0800

    [FLIR] Use main branch of flir

--- python/setup_tools/utils/__init__.py
+++ python/setup_tools/utils/__init__.py
@@ -58,12 +58,7 @@ submodule_registrar = SubmoduleRegistrar(submodules=(
         "name": "triton_shared", "url": "https://github.com/microsoft/triton-shared.git", "commit_id":
         "5842469a16b261e45a2c67fbfc308057622b03ee"
     },
-    {
-        "name": "flir", "url": "https://github.com/tsingmicro-public-e/flir.git", "commit_id":
-        "4b950511caf6930d0ba2c177d9cef2c588b96669"
-    },
-    #{"name": "flir", "url": "https://github.com/FlagTree/flir.git"},
-    # FlagPrism: register the external component without vendoring its sources.
+    {"name": "flir", "url": "https://github.com/flagos-ai/flir.git"},
     {"name": "FlagPrism", "url": "https://github.com/flagos-ai/FlagPrism.git"},
     {"name": "flagcx", "url": "https://github.com/flagos-ai/FlagCX.git", "relative_path": "tle/third_party/flagcx"},
     {
